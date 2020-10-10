b277009759e0174ab8623a20004701f603477700
mauroef <cypress90@gmail.com>
2020-10-09 23:02:59 -0300

    start with model construction

---
+++ Models/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeNetWeb.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public int Pin { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
