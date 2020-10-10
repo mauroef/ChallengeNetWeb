284f1887d080c39a9b7069e5607d7b36ad1e0f2b
mauroef <cypress90@gmail.com>
2020-10-10 02:08:05 -0300

    Setting up entity framework.

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using ChallengeNetWeb.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,6 +24,7 @@ namespace ChallengeNetWeb
         {
 
             services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ChallengeNetWeb")));
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
