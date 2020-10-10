71458a5cb8efc08ad2213d196731b4eb0010e38a
mauroef <cypress90@gmail.com>
2020-10-10 17:23:03 -0300

    Enum added.

---
+++ Helpers/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeNetWeb.Helpers
+{
+    public static class Enums
+    {
+        public enum Transaction
+        { 
+            Balance = 1,
+            Withdraw = 2
+        }
+
+        public enum Status
+        {
+            Active = 1,
+            Blocked = 2
+        }
+
+        public enum Card
+        {
+            MaxAttempt = 3
+        }
+    }
+}
